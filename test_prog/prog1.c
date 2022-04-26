5f276117a5058f0e4a96d22c3e66140207bbdd5c
buhe <bh@raspberry>
2022-04-26 02:29:45 +0100

    u

---
+++ test_prog/prog1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_pr(void) {
+
+	FILE *fp = fopen("/proc/process_sched_add","w");
+	fprintf(fp, "%d", getpid());
+ 	fclose(fp);
+
+ 	while(1) {
+ 		printf("My pid: %d\n", getpid());
+ 		sleep(1);
+ 	}
+}
+
+int main() {
+    test_pr();
+	return 0;
+}
